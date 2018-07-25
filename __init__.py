b00a207d49c7e8dbb027a9a7b89fc1f7644b044e
mrillies <jacob.gillies@gmail.com>
2018-07-25 16:06:00 +0400

    Update __init__.py
    Fixed f_inv options
    
    Corrected base use before assignment

--- __init__.py
+++ __init__.py
@@ -12,7 +12,7 @@ import traceback
 def initMCP(app):
     mcp.wiringPiSetup()
     #get mcp object if it exisits
-    #base = cbpi.get_config_parameter("MCP_wiring_Pi_Base", None)
+    base = cbpi.get_config_parameter("MCP_wiring_Pi_Base", None)
     if base is None:
         base = "500"
         #cbpi.add_config_parameter("MCP_Wiring_Pi_Base", "500", "text", "MCP Base - change is wiring pi libary conflicts occur")
@@ -26,7 +26,7 @@ class MCP23017_TestActor(ActorBase):
     a_busad = Property.Select("Bus Address", options=["0x20","0x21","0x22","0x23","0x24","0x25","0x26","0x27"], description="Bus address setting of MCP based on A0 A1 A2.")
     b_chan = Property.Text("Channel", configurable=True, default_value="100", description="MCP Output channel 0-128")
     #c_pud = Property.Select("Pull up", options=["Off","Up"], default_value = "Off", description="Pull Up or down resisitor")
-    f_inv = Property.Select("Invert Output", default_value = "No", description="Invert the output so on means output at 0V")
+    f_inv = Property.Select("Invert Output", options=["No","Yes"], description="Invert the output so on means output at 0V")
 
     #initiasliser called when an actor is created or changed
     def init(self):
